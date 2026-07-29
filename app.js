bb148029609dcdc48f1765dd0d8ea46b49d32b01
InvictusKhan <fasihkhan8352@gmail.com>
2026-07-29 08:46:37 +0400

    Bare Server Project Done

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-console.log(`Hello World`);
+import express from "express"
+const PORT = process.env.PORT;
+
+const app = express();
+
+app.get('/api', (req, res) => {
+    res.send('Server is Up')
+});
+
+app.listen(PORT, () =>{
+    console.log(`Server is live on port ${PORT}`);
+})
