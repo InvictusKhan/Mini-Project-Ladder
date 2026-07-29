f6b6c9e89774c56b1b45ca34507ccd78c6a54cdb
InvictusKhan <fasihkhan8352@gmail.com>
2026-07-29 14:30:19 +0400

    Connected and tested Database connection

--- routes/router.js
+++ routes/router.js
@@ -1,11 +1,21 @@
 import express from 'express';
+import pool from '../db.js';
 import logger from '../middleware/logger.js';
 const router = express.Router();
 
-router.get('/api', (req, res) => {
-    res.send('Home Page');
-    
+router.get('/api', async (req, res) => {
+  try {
+    const result = await pool.query('SELECT NOW()');
+    res.json(result.rows);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Database query failed' });
+  }
 });
+
+router.get('/api/posts', (req, res) => {
+    res.send('Posts Page');
+})
 
 
 export default router;
